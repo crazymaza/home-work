20f64bb6980bda54b4f4afc91f7e023e974591ec
IValnev <dx2389@gmail.com>
2021-11-15 20:15:35 +0300

    Created classes for lesson #3.

---
+++ src/main/java/com/sbrf/reboot/dao/Account.java
@@ -0,0 +1,10 @@
+package com.sbrf.reboot.dao;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class Account {
+    private final String clientId;
+}
