3e1920cb2b34da377da869600da85adc3dbba4b2
Igor Valnev <iavalnev@sberbank.ru>
2021-11-28 17:15:04 +0300

    Убрал неиспользуемый импорт.

--- src/main/java/com/sbrf/reboot/dao/Account.java
+++ src/main/java/com/sbrf/reboot/dao/Account.java
@@ -3,7 +3,6 @@ package com.sbrf.reboot.dao;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
